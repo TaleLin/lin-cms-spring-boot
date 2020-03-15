1cdfe4bcc30ffff1657409fbdfce5b745f7da1a2
pedro <gaopedro@163.com>
2020-03-15 22:29:05 +0800

    feat:删除无用的代码段

--- src/main/java/io/github/talelin/merak/modules/message/WsHandlerImpl.java
+++ src/main/java/io/github/talelin/merak/modules/message/WsHandlerImpl.java
@@ -39,7 +39,7 @@ public class WsHandlerImpl implements WsHandler {
 
     @Override
     public void handleMessage(WebSocketSession session, String message) {
-        log.info("from {}-{}'s message：{}", session.getId(), session.getAttributes().get("username"), message);
+        // 只处理前端传来的文本消息，并且直接丢弃了客户端传来的消息
     }
 
     @Override
