e1cfbebaa50725b02692fa5b11f07cf1c603065c
桔子 <juzi214032@qq.com>
2020-05-18 14:49:08 +0800

    refactor(Uploader): 重构 Uploader 实例注入方式

---
+++ src/main/java/io/github/talelin/latticy/extension/file/UploaderConfiguration.java
@@ -0,0 +1,12 @@
+package io.github.talelin.latticy.extension.file;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 文件上传配置类
+ *
+ * @author Juzi@TaleLin
+ */
+@Configuration
+public class UploaderConfiguration {
+}
