31089b531bfabcdfa5c2a201b2bc16335d2218aa
桔子 <juzi214032@qq.com>
2020-05-19 18:32:33 +0800

    refactor(Length): 修改 @Length 注解为 Hibernate 注解

--- src/main/java/io/github/talelin/latticy/dto/user/UpdateInfoDTO.java
+++ src/main/java/io/github/talelin/latticy/dto/user/UpdateInfoDTO.java
@@ -1,7 +1,7 @@
 package io.github.talelin.latticy.dto.user;
 
-import io.github.talelin.autoconfigure.validator.Length;
 import lombok.*;
+import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.Email;
 
