a8f113455bdd5065a3d6d1caed36b08a948f90bd
桔子 <juzi214032@qq.com>
2020-06-25 14:06:48 +0800

    feat(Logback): 增强 Logback 功能
    1.将不同级别日志分文件夹输出
    2.基于 ServletFliter 实现 Access Log

--- src/main/java/io/github/talelin/latticy/common/configuration/CommonConfiguration.java
+++ src/main/java/io/github/talelin/latticy/common/configuration/CommonConfiguration.java
@@ -5,10 +5,12 @@ import com.baomidou.mybatisplus.core.injector.ISqlInjector;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import io.github.talelin.autoconfigure.bean.PermissionMetaCollector;
-import io.github.talelin.latticy.module.file.FileProperties;
 import io.github.talelin.latticy.common.interceptor.RequestLogInterceptor;
+import io.github.talelin.latticy.module.file.FileProperties;
+import io.github.talelin.latticy.module.log.MDCAccessServletFilter;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -55,5 +57,19 @@ public class CommonConfiguration {
             jacksonObjectMapperBuilder.failOnUnknownProperties(false);
             jacksonObjectMapperBuilder.propertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
         };
+    }
+
+    /**
+     * 用于将 request 相关信息（如请求 url）放入 MDC 中供日志使用
+     *
+     * @return Logback 的 MDCInsertingServletFilter
+     */
+    @Bean
+    public FilterRegistrationBean<MDCAccessServletFilter> mdcInsertingServletFilter() {
+        FilterRegistrationBean<MDCAccessServletFilter> filterRegistrationBean = new FilterRegistrationBean<>();
+        MDCAccessServletFilter mdcAccessServletFilter = new MDCAccessServletFilter();
+        filterRegistrationBean.setFilter(mdcAccessServletFilter);
+        filterRegistrationBean.setName("mdc-access-servlet-filter");
+        return filterRegistrationBean;
     }
 }
