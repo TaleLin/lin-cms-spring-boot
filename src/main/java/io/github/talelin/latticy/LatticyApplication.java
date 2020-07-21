91c415dedbb33faf1179c65687dff2585afb0ee6
Colorful3 <fujiale3@qq.com>
2020-07-21 15:34:54 +0800

    feat: 集成 Prometheus

--- src/main/java/io/github/talelin/latticy/LatticyApplication.java
+++ src/main/java/io/github/talelin/latticy/LatticyApplication.java
@@ -1,8 +1,12 @@
 package io.github.talelin.latticy;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +20,11 @@ public class LatticyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LatticyApplication.class, args);
+    }
+
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> configurer(@Value("${spring.application.name}") String applicationName) {
+        return (registry) -> registry.config().commonTags("application", applicationName);
     }
 
     @RequestMapping("/")
