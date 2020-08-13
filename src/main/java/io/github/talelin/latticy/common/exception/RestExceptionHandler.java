f71f542f06c5e631c7f7b78bb97b54140992dba8
fujiale33 <fujiale3@qq.com>
2020-08-13 15:16:16 +0800

    chore: 删除多余的包导入

--- src/main/java/io/github/talelin/latticy/common/exception/RestExceptionHandler.java
+++ src/main/java/io/github/talelin/latticy/common/exception/RestExceptionHandler.java
@@ -26,7 +26,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.ConstraintViolationException;
-import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.List;
