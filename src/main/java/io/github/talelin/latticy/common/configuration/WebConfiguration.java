26badd08042134088bd4851c7598a739b8f0a1ab
fujiale33 <fujiale3@qq.com>
2020-12-23 17:05:05 +0800

    fix: 修复升级 spring-boot 2.4.0 后导致的跨域问题

--- src/main/java/io/github/talelin/latticy/common/configuration/WebConfiguration.java
+++ src/main/java/io/github/talelin/latticy/common/configuration/WebConfiguration.java
@@ -55,7 +55,7 @@ public class WebConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true)
                 .maxAge(3600)
