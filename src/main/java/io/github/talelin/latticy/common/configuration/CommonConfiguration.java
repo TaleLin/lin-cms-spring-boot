ec67526d8073b3c44e9b80033d05a564ce1fbea7
桔子 <juzi214032@qq.com>
2021-02-05 15:14:08 +0800

    fix(MyBatisPlus): 修复分页失效的问题
    close #175

--- src/main/java/io/github/talelin/latticy/common/configuration/CommonConfiguration.java
+++ src/main/java/io/github/talelin/latticy/common/configuration/CommonConfiguration.java
@@ -1,8 +1,12 @@
 package io.github.talelin.latticy.common.configuration;
 
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer;
 import com.baomidou.mybatisplus.core.injector.DefaultSqlInjector;
 import com.baomidou.mybatisplus.core.injector.ISqlInjector;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import io.github.talelin.autoconfigure.bean.PermissionMetaCollector;
 import io.github.talelin.latticy.common.interceptor.RequestLogInterceptor;
@@ -26,9 +30,24 @@ public class CommonConfiguration {
         return new RequestLogInterceptor();
     }
 
+    /**
+     * 新的分页插件,一缓和二缓遵循mybatis的规则
+     * 需要设置 MybatisConfiguration#useDeprecatedExecutor = false 避免缓存出现问题(该属性会在旧插件移除后一同移除)
+     * 参考链接：https://mp.baomidou.com/guide/interceptor.htm
+     */
     @Bean
-    public PaginationInterceptor paginationInterceptor() {
-        return new PaginationInterceptor();
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+
+    /**
+     * 参考链接：https://mp.baomidou.com/guide/interceptor.htm
+     */
+    @Bean
+    public ConfigurationCustomizer configurationCustomizer() {
+        return configuration -> configuration.setUseDeprecatedExecutor(false);
     }
 
     @Bean
