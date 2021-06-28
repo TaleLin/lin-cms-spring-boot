4b0d9b58a2308b9bf97e22e6d2ffd69ea2709932
Colorful <fujiale3@qq.com>
2021-06-28 20:48:55 +0800

    refactor: 使用dto代替接口的查询参数形参列表

--- src/main/java/io/github/talelin/latticy/dto/query/BasePageDTO.java
+++ src/main/java/io/github/talelin/latticy/dto/query/BasePageDTO.java
@@ -14,9 +14,23 @@ public class BasePageDTO {
 
     @Min(value = 1, message = "{page.count.min}")
     @Max(value = 30, message = "{page.count.max}")
-    private Integer count = 10;
+    private Integer count;
 
     @Min(value = 0, message = "{page.number.min}")
-    private Integer page = 0;
+    private Integer page;
+
+    public Integer getCount() {
+        if (null == count) {
+            return 10;
+        }
+        return count;
+    }
+
+    public Integer getPage() {
+        if (null == page) {
+            return 0;
+        }
+        return page;
+    }
 
 }
