04ba8b903898a33c472071e71d442b9ce83dd863
Gadfly <gadfly@gadfly.vip>
2021-07-14 18:01:48 +0800

    fix #223 & test去除deprecated api等问题修复

--- src/main/java/io/github/talelin/latticy/vo/UpdatedVO.java
+++ src/main/java/io/github/talelin/latticy/vo/UpdatedVO.java
@@ -11,22 +11,22 @@ public class UpdatedVO extends UnifyResponseVO<String> {
 
     public UpdatedVO() {
         super(Code.UPDATED.getCode());
-        ResponseUtil.setCurrentResponseHttpStatus(HttpStatus.CREATED.value());
+        ResponseUtil.setCurrentResponseHttpStatus(HttpStatus.OK.value());
     }
 
     public UpdatedVO(int code) {
         super(code);
-        ResponseUtil.setCurrentResponseHttpStatus(HttpStatus.CREATED.value());
+        ResponseUtil.setCurrentResponseHttpStatus(HttpStatus.OK.value());
     }
 
     public UpdatedVO(String message) {
         super(message);
-        ResponseUtil.setCurrentResponseHttpStatus(HttpStatus.CREATED.value());
+        ResponseUtil.setCurrentResponseHttpStatus(HttpStatus.OK.value());
     }
 
     public UpdatedVO(int code, String message) {
         super(code, message);
-        ResponseUtil.setCurrentResponseHttpStatus(HttpStatus.CREATED.value());
+        ResponseUtil.setCurrentResponseHttpStatus(HttpStatus.OK.value());
     }
 
     @Override
